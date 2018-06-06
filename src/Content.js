4cbf8f168a8bf06b9ad136a9f5ad348faaeca53a
Matt Chapman <matt@mttchpmn.com>
2018-06-06 18:02:43 +1200

    Add CV Content file

---
+++ src/Content.js
@@ -0,0 +1,67 @@
+const content = {
+    profile: {
+        name: 'Matt Chapman',
+        tag: 'developer // creative // adventurer', 
+        bio: 'I am a creative and driven developer with a passion to learn and a thirst for knowledge.',
+        image: 'https://avatars1.githubusercontent.com/u/26560482',
+        contact: [
+            {
+                type: 'email',
+                value: 'matt@mattchapman.io'
+            },
+            {
+                type: 'phone',
+                value: '+64 27 268 2427',
+            },
+            {
+                type: 'website',
+                value: 'http://mattchapman.io',
+            },
+            {
+                type: 'github',
+                value: 'https://github.com/mttchpmn',
+            }
+        ]
+    },
+    stack: [
+        {
+            name: 'Javascript',
+            competencyLevel: 60,
+            yearsExperience: 0
+        },
+        {
+            name: 'NodeJS',
+            competencyLevel: 65,
+            yearsExperience: 0
+        },
+        {
+            name: 'Python',
+            competencyLevel: 70,
+            yearsExperience: 0
+        },
+        {
+            name: 'Linux',
+            competencyLevel: 85,
+            yearsExperience: 0
+        },
+    ],
+    skills: [
+        'ITIL IT Technician (Level 1 & 2)',
+        'AS/NZS 3760:2012 Electrical Tag and Test Competency',
+        'Freelance photographer, videographer, and editor',
+        'Solo live performance musician and vocalist',
+        'LTSA Class 1 and 2 Licences with P endorsement',
+        'NZ CAA Private Pilot (Aeroplane)',
+        'NZHGPA PG2 Paraglider Pilot',
+        'PADI Rescue Diver; Nitrox & Drysuit rated',
+        'NZRA Grade 2 Raft Guide',
+        'NZRA Grade 3 Swiftwater Rescue',
+        'Land Search and Rescue Operational Field Team Member',
+        'PADI / Red Cross Emergency First Responder'
+
+    ],
+    education: {},
+    experience: {}
+};
+
+export default content;
